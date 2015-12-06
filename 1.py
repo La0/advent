f05062daf39c1f4f7c748945207fd8e8ae1094a2
Bastien Abadie <bastien.abadie@gmail.com>
2015-12-06 13:49:04 +0100

    Second half of day 1

--- 1.py
+++ 1.py
@@ -1,14 +1,25 @@
+floors = {
+    ')' : -1,
+    '(' : 1,
+}
+
 def calc_floor(line):
     """
     Start on floor 0
     An opening parenthesis, (, means he should go up one floor, and a closing parenthesis, ), means he should go down one floor.
     """
-    floors = {
-        ')' : -1,
-        '(' : 1,
-    }
     return sum([floors.get(c, 0) for c in line])
+
+def find_basement(line):
+    """
+    Same rules, find first position at -1
+    """
+    floor = 0
+    for i, c in enumerate(line):
+        floor += floors.get(c, 0)
+        if floor == -1:
+            return i+1
 
 if __name__ == '__main__':
     with open('1.input', 'r') as f:
-        print calc_floor(f.read())
+        print find_basement(f.read())
