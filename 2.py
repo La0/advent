28a8c26b4017c749265571d0ba3bc5457d2015ec
Bastien Abadie <bastien.abadie@gmail.com>
2015-12-06 20:35:47 +0100

    Second half o day 2

--- 2.py
+++ 2.py
@@ -2,6 +2,11 @@ def calc_wrapping(line):
     l,w,h = map(int, line.split('x'))
     return 2*l*w + 2*w*h + 2*h*l + min(l*w, w*h, h*l)
 
+def calc_ribbon(line):
+    l,w,h = map(int, line.split('x'))
+
+    return 2 * min(l+w, w+h, l+h) + l*w*h
+
 if __name__ == '__main__':
     with open('2.input', 'r') as f:
-        print sum([calc_wrapping(l.replace('\n', '')) for l in f.readlines()])
+        print sum([calc_ribbon(l.replace('\n', '')) for l in f.readlines()])
