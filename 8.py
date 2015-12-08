d17c6a3c17cc5b23dc5311390e8a7faec9f5b397
Bastien Abadie <bastien.abadie@gmail.com>
2015-12-08 18:48:54 +0100

    Second half of day 8

--- 8.py
+++ 8.py
@@ -2,7 +2,6 @@ import re
 
 def calc_length(line):
 
-
     # Code
     total_code = len(line)
 
@@ -28,6 +27,31 @@ def calc_length(line):
     print ' > memory', total_memory
     return total_code - total_memory
 
+def calc_encoded(line):
+    # Code
+    total_code = len(line)
+
+    # Encode the line
+    line = r''.join(line)
+    print line
+    line = line.replace('\\', r'\\')
+
+    # * utf8
+    line = re.sub(r'\\x([0-9a-fA-F]{2})', r'\\x\1', line)
+
+    # * quotes
+    line = line.replace('"', r'\"')
+
+    # Add enclosing quotes
+    line = '"' + line + '"'
+
+    total_encoded = len(line)
+    print line
+    print 'code', total_code
+    print 'encoded', total_encoded
+
+    return total_encoded - total_code
+
 if __name__ == '__main__':
     total = 0
     with open('8.input', 'rU') as f:
@@ -40,7 +64,7 @@ if __name__ == '__main__':
                 if c == '':
                     break
                 else:
-                    total += calc_length(line)
+                    total += calc_encoded(line)
                     line = []
                     continue
             line.append(c)
