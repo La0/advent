462489dfd430db4f0bfd365928925892a904e197
Bastien Abadie <bastien.abadie@gmail.com>
2015-12-16 12:33:47 +0100

    First half of day 16.

---
+++ 16.py
@@ -0,0 +1,40 @@
+import re
+
+detected = {
+    'children' : 3,
+    'cats' : 7,
+    'samoyeds' : 2,
+    'pomeranians' : 3,
+    'akitas' : 0,
+    'vizslas' : 0,
+    'goldfish' : 5,
+    'trees' : 3,
+    'cars' : 2,
+    'perfumes' : 1,
+}
+
+def find_sue(lines):
+    aunts = {}
+
+    # Extract data about all aunts
+    regex = re.compile(r'^Sue (\d+): (.*)\n$')
+    regex_props = re.compile(r'(\w+): (\d+)')
+    for line in lines:
+        res = regex.match(line)
+        name, properties = res.groups()
+        aunts[int(name)] = dict(map(lambda x : (x[0],int(x[1])),  regex_props.findall(properties)))
+
+
+    # Find closest aunt
+    best_distance = 1000
+    for aunt, props in aunts.items():
+
+        # Calc distance
+        distance = sum([abs(v - detected[k]) for k,v in props.items()])
+        best_distance = min(distance, best_distance)
+        if distance == best_distance:
+            print aunt, props, distance
+
+if __name__ == '__main__':
+    with open('16.input', 'r') as f:
+        find_sue(f.readlines())
