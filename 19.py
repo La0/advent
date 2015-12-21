a0765b04f6229d56abc16b51e7f442691dbcab76
Bastien Abadie <bastien.abadie@gmail.com>
2015-12-21 18:42:36 +0100

    Second half of day 19.

--- 19.py
+++ 19.py
@@ -1,4 +1,5 @@
 import re
+import random
 
 class Machine(object):
     """
@@ -6,7 +7,6 @@ class Machine(object):
     """
     def __init__(self, filename):
         regex = re.compile(r'^(\w+) => (\w+)\n$')
-        self.calibrations = set()
 
         # Load input
         self.replace = {}
@@ -17,40 +17,78 @@ class Machine(object):
                 res = regex.match(line)
                 if res:
                     a, b = res.groups()
-                    if a not in self.replace:
-                        self.replace[a] = []
-                    self.replace[a].append(b)
+                    self.replace[b] = a
                 else:
-                    self.start = line.replace('\n', '')
+                    self.target = line.replace('\n', '')
 
-    def calibrate(self):
+    def calibrate(self, source):
         """
         Find all molecules generated in one step
         """
+        if source.count('e') > 1:
+            return # optim
+
+        calibrations = set()
         for atom, replace in self.replace.items():
             l = len(atom)
 
-            for i in range(0, len(self.start) - l + 1):
-                c = self.start[i:i+l]
+            for i in range(0, len(source) - l + 1):
+                c = source[i:i+l]
 
                 if c != atom:
                     continue
 
-                for r in replace:
-                    self.add_replacement(i, atom, r)
+                s = list(source)
+                s = s[:i] + [self.replace[atom], ] + s[i+len(atom):]
+                out = ''.join(s)
+                calibrations.add(out)
 
-        return len(self.calibrations)
 
-    def add_replacement(self, pos, src, dest):
-        s = list(self.start)
-        s = s[:pos] + [dest, ] + s[pos+len(src):]
-        out = ''.join(s)
-        print '#%d %s => %s = %s' % (pos, src, dest, out)
-        self.calibrations.add(out)
+        return calibrations
 
+    def build(self):
+        """
+        List all possible combinations to build target
+        """
+        all_steps = []
+
+        def _search(source, steps):
+            if not len(source):
+                return
+            if source == 'e':
+                print 'FOUND!', steps
+                all_steps.append(int(steps))
+                return
+
+            calib = self.calibrate(source)
+            if not calib:
+                return
+
+            for c in calib:
+                _search(c, steps+1)
+
+        _search(self.target, 0) # start from electron
+        return min(all_steps)
+
+    def solve(self):
+        """
+        Try solving target by substituting all chars
+        """
+        keys = self.replace.keys()
+
+        cpt = 0
+        while self.target != 'e':
+            random.shuffle(keys)
+            for key in keys:
+                replace = self.replace[key]
+                self.target, n = re.subn(key, replace, self.target)
+                if n > 0:
+                    print key, replace, self.target
+                cpt += n
+
+        return cpt
 
 
 if __name__ == '__main__':
-    m = Machine('19.test')
     m = Machine('19.input')
-    print m.calibrate()
+    print m.solve()
