9735149166de7e106ddb17ab0c82310182dd7d4e
Bastien Abadie <bastien@nextcairn.com>
2016-12-07 09:25:27 +0100

    day 3, part 2

--- 2016/3.py
+++ 2016/3.py
@@ -1,18 +1,28 @@
 import re
 
 
-def solve(lines):
-    out = len(lines)
-    for line in lines:
-        sides = map(int, re.findall('(\d+)', line))
-        total = sum(sides)
-        for i in range(3):
-            remaining = sides[i]
-            if (total - remaining) <= remaining:
-                out -= 1
-                break
+def is_triangle(sides):
+    total = sum(sides)
+    for i in range(3):
+        remaining = sides[i]
+        if (total - remaining) <= remaining:
+            return False
+    return True
 
-    return out
+
+def solve(lines):
+    grid = [map(int, re.findall('(\d+)', line)) for line in lines]
+    triangles = []
+    for i in range(len(grid)):
+        x = i % 3
+        y = (i / 3) * 3 # 3 by 3
+        triangles.append([
+            grid[y][x],
+            grid[y+1][x],
+            grid[y+2][x],
+        ])
+
+    return sum(map(is_triangle, triangles))
 
 if __name__ == '__main__':
     with open('3.txt') as f:
