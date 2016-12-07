80a1be4de28ea80621a58cf32a527c98553d6177
Bastien Abadie <bastien@nextcairn.com>
2016-12-07 14:22:32 +0100

    day 7, part 2

--- 2016/7.py
+++ 2016/7.py
@@ -1,3 +1,4 @@
+import itertools
 import re
 
 def is_abba(line):
@@ -23,6 +24,33 @@ def is_tls(address):
 
     return True
 
+def get_aba(line):
+    out = []
+    for i in range(len(line) - 2):
+        aba = line[i:i+3]
+        if aba[0] == aba[2] and aba[0] != aba[1]:
+            out.append(aba)
+    return out
+
+def has_bab(line, aba):
+    bab = aba[1] + aba[0] + aba[1]
+    return bab in line
+
+def is_ssl(address):
+    # Split parts
+    hypernets = re.findall(r'\[(\w+)\]', address)
+    supernets = re.split('\[\w+\]', address)
+
+    abas = sum(map(get_aba, supernets), [])
+    if not abas:
+        return False
+
+    out = [
+        has_bab(h, aba)
+        for h, aba in itertools.product(hypernets, abas)
+    ]
+    return True in out
+
 
 if __name__ == '__main__':
     assert is_tls('abba[mnop]qrst')
@@ -32,4 +60,13 @@ if __name__ == '__main__':
 
     with open('7.txt') as f:
         x = sum(map(is_tls, f.readlines()))
+        #print(x)
+
+    assert is_ssl('aba[bab]xyz')
+    assert not is_ssl('xyx[xyx]xyx')
+    assert is_ssl('aaa[kek]eke')
+    assert is_ssl('zazbz[bzb]cdb')
+
+    with open('7.txt') as f:
+        x = sum(map(is_ssl, f.readlines()))
         print(x)
