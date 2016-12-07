0b4daf97d910ab00ccee29381684b9adcb11c145
Bastien Abadie <bastien@nextcairn.com>
2016-12-07 10:29:35 +0100

    day 5, part 2

--- 2016/5.py
+++ 2016/5.py
@@ -2,16 +2,22 @@ from hashlib import md5
 
 def crack(door_id):
     index = 0
-    password = ''
-    while len(password) < 8:
+    password = [' '] * 8
+    while ' ' in password:
         contents = door_id + str(index)
         h = md5(contents).hexdigest()
         if h[:5] == '00000':
             print(index, contents, h)
-            password += h[5]
+            try:
+                pos = int(h[5])
+                if password[pos] != ' ':
+                    raise Exception('already filled {}'.format(pos))
+                password[pos] = h[6]
+            except Exception as e:
+                print('error', e)
         index += 1
 
-    return password
+    return ''.join(password)
 
 
 if __name__ == '__main__':
