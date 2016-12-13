c227ee23618877036fd10fd2a532f65c2cd5417c
Bastien Abadie <bastien@nextcairn.com>
2016-12-13 13:19:45 +0100

    day 13, part 1

--- 2016/13.py
+++ 2016/13.py
@@ -14,38 +14,37 @@ def distance(ax, ay, bx, by):
 def solve(final_x, final_y, favorite_number):
 
     done = []
-    queue = [(0,0,distance(0, 0, final_x, final_y), 0)] # start
-    solutions = []
+    queue = [(1,1,distance(1, 1, final_x, final_y), 0)] # start
+    moves = (
+        (-1, 0),
+        (1, 0),
+        (0, -1),
+        (0, 1),
+    )
     while len(queue) > 0:
         x,y,d,s = queue.pop(0)
 
         if d == 0:
-            solutions.append(s)
-            print(solutions)
+            return s
         done.append((x,y))
 
         # Add neighbors to queue
-        for i in range(-1, 2):
-            for j in range(-1, 2):
-                if (i,j) == (0,0):
-                    continue # current pos
-                nx, ny = x+i, y+j
-                if (nx,ny) in done:
-                    continue # already passed
-                done.append((nx, ny))
-                wall = is_wall(nx, ny, favorite_number)
-                if wall is None or wall is True:
-                    continue # invalid position
-                d = distance(nx, ny, final_x, final_y)
-                queue.append((nx, ny, d, s + abs(i) + abs(j)))
-                #print(nx, ny, wall)
-
-    print(solutions)
-    return min(solutions)
+        for i, j in moves:
+            nx, ny = x+i, y+j
+            if (nx,ny) in done:
+                continue # already passed
+            done.append((nx, ny))
+            wall = is_wall(nx, ny, favorite_number)
+            if wall is None or wall is True:
+                continue # invalid position
+            d = distance(nx, ny, final_x, final_y)
+            queue.append((nx, ny, d, s + abs(i) + abs(j)))
+            #print(nx, ny, wall)
 
 
 if __name__ == '__main__':
     assert not is_wall(0,0, 10)
     assert ''.join([is_wall(i, 0, 10) and '#' or '.' for i in range(10)]) == '.#.####.##'
-    x = solve(7,4, 10)
+    assert solve(7, 4, 10) == 11
+    x = solve(31,39,1352)
     print(x)
